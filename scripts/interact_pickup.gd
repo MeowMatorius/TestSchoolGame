9e5642ef7b832693bff6e608d017fd4cce93a796
MeowMatorious <No email>
2026-04-24 11:26:08 +0300

    Теперь точно пофиксил prompt

--- scripts/interact_pickup.gd
+++ scripts/interact_pickup.gd
@@ -11,8 +11,9 @@ signal object_removed
 
 func interact(object):
 	SignalBus.is_picking.emit(pickup_item_name, pickup_item_icon, pickup_item_quantity, pickup_item_unique, pickup_item_type)
-	print("\n", get_script().resource_path.get_file(), ":\n", "Игрок подобрал ", name)
+	print("\n", get_script().resource_path.get_file(), ":\n", "Игрок подобрал ", get_parent().name)
 	
+	get_parent().collision_layer = 0
 	object_removed.emit()
 	get_parent().queue_free()
 
