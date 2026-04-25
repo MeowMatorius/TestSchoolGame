f3df32f6741658b203342116806f6ba949a751f5
VIV2020 <none@idi.naxuy>
2026-04-25 17:22:16 +0300

    Разрешен конфликт

--- player/ui/player_ui_dialogues.gd
+++ player/ui/player_ui_dialogues.gd
@@ -9,18 +9,12 @@ extends Control
 
 func _ready() -> void:
 	DialogueManager.started_talking.connect(show_ui)
-	DialogueManager.stoped_talking.connect(hide_ui)
 	DialogueManager.start_choosing.connect(show_choices)
 
 
 func show_ui(speaker, line):
-	dialogue_ui.visible = true
 	speaker_name.text = speaker
 	speaker_line.text = line
-	
-	
-func hide_ui():
-	dialogue_ui.visible = false
 
 	
 func show_choices(choices_array):
