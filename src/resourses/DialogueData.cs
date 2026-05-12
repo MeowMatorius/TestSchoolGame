c06150a589c5cacaea864702ec1072d063741ea0
MeowMatorious <65506085+MeowMatorius@users.noreply.github.com>
2026-05-13 00:20:22 +0300

    Удален мусор из репозитория согласно .gitignore

--- src/resourses/DialogueData.cs
+++ src/resourses/DialogueData.cs
@@ -10,23 +10,17 @@ public partial class DialogueData : Resource
         Negative
     }
 
-    [Export] 
-    public string CharacterName { get; set; }
+    [Export] public string CharacterName { get; set; }
 
-    [Export(PropertyHint.MultilineText)] 
-    public string Text { get; set; }
+    [Export(PropertyHint.MultilineText)] public string Text { get; set; }
 
-    [Export] 
-    public DialogueData NextDialogue { get; set; }
+    [Export] public DialogueData NextDialogue { get; set; }
 
-    [Export] 
-    public Array<DialogueData> Choices { get; set; } = new();
+    [Export] public Array<DialogueData> Choices { get; set; } = new();
 
-    [Export] 
-    public Array<ConditionData> Condition { get; set; } = new();
+    [Export] public Array<ConditionData> Condition { get; set; } = new();
 
-    [Export] 
-    public Array<QuestData> Quest { get; set; } = new();
+    [Export] public Array<QuestData> Quest { get; set; } = new();
 
-    [Export] 
-    public AnswerType Answer { get; set; } = AnswerType.Positive;
+    [Export] public AnswerType Answer { get; set; } = AnswerType.Positive;
+}
