95f58637a46bbb9150d9d2a78fe6b019d4012a3b
MeowMatorious <65506085+MeowMatorius@users.noreply.github.com>
2026-08-23 06:59:22 +0300

    Изменение сцены, тест LUT, шейдеры, сжатие

--- src/scripts/interact/interact.gd
+++ src/scripts/interact/interact.gd
@@ -10,18 +10,20 @@ func _ready() -> void:
 
 
 func highlight(object_mesh):
-	if object_mesh is MeshInstance3D and !highlighted:
-		highlighted = true
-		var mat: Material = object_mesh.get_active_material(0)
-		if mat is StandardMaterial3D:
-			mat.emission_enabled = true
-			mat.emission = Color(1.0, 1.0, 1.0, 1.0) # Зеленая подсветка
-			mat.emission_energy_multiplier = 0.05
+	pass
+	#if object_mesh is MeshInstance3D and !highlighted:
+	#	highlighted = true
+	#	var mat: Material = object_mesh.get_active_material(0)
+	#	if mat is StandardMaterial3D:
+	#		mat.emission_enabled = true
+	#		mat.emission = Color(1.0, 1.0, 1.0, 1.0) # Зеленая подсветка
+	#		mat.emission_energy_multiplier = 0.05
 
 
 func disable_highlight(object_mesh):
-	if object_mesh is MeshInstance3D and highlighted:
-		highlighted = false
-		var mat: Material = object_mesh.get_active_material(0)
-		if mat is StandardMaterial3D:
-			mat.emission_enabled = false
+	pass
+	#if object_mesh is MeshInstance3D and highlighted:
+	#	highlighted = false
+	#	var mat: Material = object_mesh.get_active_material(0)
+	#	if mat is StandardMaterial3D:
+	#		mat.emission_enabled = false
